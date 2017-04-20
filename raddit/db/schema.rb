0bbf7a52f6c5570d19815801fa684f600d3f959b
moyuanhuang <moyuanhuang@gmail.com>
2017-04-20 15:34:17 -0700

    add association between link and user.

--- raddit/db/schema.rb
+++ raddit/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170413073126) do
+ActiveRecord::Schema.define(version: 20170420222920) do
 
   create_table "installs", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
@@ -36,7 +36,10 @@ ActiveRecord::Schema.define(version: 20170413073126) do
     t.string   "url"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer  "user_id"
   end
+
+  add_index "links", ["user_id"], name: "index_links_on_user_id"
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
