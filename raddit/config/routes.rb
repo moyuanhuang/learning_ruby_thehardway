4f56b6f96ba88b4317b7efdbac98bbb3ca80dd21
moyuanhuang <moyuanhuang@gmail.com>
2017-05-17 15:07:48 -0700

    add comments

--- raddit/config/routes.rb
+++ raddit/config/routes.rb
@@ -1,10 +1,12 @@
 Rails.application.routes.draw do
+  resources :comments
   devise_for :users
   resources :links do
     member do
       put 'like',   to: "links#upvote"
       put 'dislike',  to: "links#downvote"
     end
+    resources :comments
   end
 
   root to: "links#index"
@@ -43,8 +45,7 @@ Rails.application.routes.draw do
 
   # Example resource route with more complex sub-resources:
   #   resources :products do
-  #     resources :comments
-  #     resources :sales do
+  #     #     resources :sales do
   #       get 'recent', on: :collection
   #     end
   #   end
--- raddit/db/schema.rb
+++ raddit/db/schema.rb
@@ -11,7 +11,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170421064254) do
+ActiveRecord::Schema.define(version: 20170509153027) do
+
+  create_table "comments", force: :cascade do |t|
+    t.integer  "link_id"
+    t.text     "body"
+    t.integer  "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_index "comments", ["link_id"], name: "index_comments_on_link_id"
+  add_index "comments", ["user_id"], name: "index_comments_on_user_id"
 
   create_table "installs", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
