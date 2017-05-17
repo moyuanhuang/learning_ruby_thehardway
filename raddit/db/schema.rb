d8ad2cc5b2f910fdccf619a915466fe072d2bd09
moyuanhuang <moyuanhuang@gmail.com>
2017-05-17 15:44:56 -0700

    add username

--- raddit/db/schema.rb
+++ raddit/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170509153027) do
+ActiveRecord::Schema.define(version: 20170517221126) do
 
   create_table "comments", force: :cascade do |t|
     t.integer  "link_id"
@@ -65,6 +65,7 @@ ActiveRecord::Schema.define(version: 20170509153027) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
+    t.string   "name"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
